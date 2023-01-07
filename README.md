4499ef2f5d6fa1617cadef68f2cb6d043f0b34fb
Axorax <78349410+Axorax@users.noreply.github.com>
2023-01-07 12:06:12 +0600

    Update site

---
+++ README.md
@@ -0,0 +1,12 @@
+<h1 align="center">Portfolio website 3</h1>
+
+### Preview Images
+
+![Website Preview Image](./preview-images/preview-1.png)
+
+![Website Preview Image](./preview-images/preview-2.png)
+
+---
+
+[Support me on Patreon](https://www.patreon.com/axorax) - 
+[Check out my socials](https://github.com/axorax/socials)
--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-const popup = document.querySelector(".popup");
+// ::     Settings    ::
+const DISCORD_NAME_AND_TAG = "Axorax#3184";
+const POPUP_DURATION = 2000;
+// ::       End       ::
 
+const popup = document.querySelector(".popup");
 const discord = document.querySelector("#discord");
 
 discord.addEventListener("click", () => {
-  navigator.clipboard.writeText("Axorax#3184");
+  navigator.clipboard.writeText(DISCORD_NAME_AND_TAG);
   popup.style.transform = "translate(0%)";
   setTimeout(() => {
     popup.style.transform = "translate(300%)";
-  }, 2000);
+  }, POPUP_DURATION);
 });
 
 document.querySelector(".close").addEventListener("click", () => {
